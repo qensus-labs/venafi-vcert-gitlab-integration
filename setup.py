4aceb8f079ba0334b4658c648982d4aa119a7df8
Hongli Lai <h.lai@fullstaq.com>
2021-04-12 12:38:12 +0200

    Put version into a text file

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
+import os.path
 
 with open('README.md', 'r', encoding='UTF-8') as f:
     long_description = f.read()
 
+version_txt_path = os.path.join('venafi_vcert_gitlab_integration', 'version.txt')
+with open(version_txt_path, 'r', encoding='UTF-8') as f:
+    version = f.read().strip()
+
 setuptools.setup(
     name='venafi-vcert-gitlab-integration',
-    version='0.9.0',
+    version=version,
     license='Apache 2.0',
     author='Fullstaq',
     author_email='info@fullstaq.com',
@@ -14,7 +19,11 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url="https://github.com/fullstaq-labs/venafi-vcert-gitlab-integration",
     platforms='any',
+    zip_safe=False,  # we require version.txt
     packages=['venafi_vcert_gitlab_integration'],
+    package_data={
+        'venafi_vcert_gitlab_integration': ['*.txt'],
+    },
     entry_points={
         'console_scripts': [
             'venafi-vcert-request-certificate=venafi_vcert_gitlab_integration.request_certificate_command:main',  # noqa:E501
